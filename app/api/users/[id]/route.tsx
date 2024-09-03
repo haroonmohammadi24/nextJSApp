170766a2d3126fc26210add30d4ba02a1d8027ec
haroonmohammadi24 <128375681+haroonmohammadi24@users.noreply.github.com>
2024-09-03 16:58:24 +0430

    working with the prisma db API and CRUD create Objects GetAll Entities and GetONEEntity by Prisma ORM and Postgres db

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -3,18 +3,44 @@
 import { error } from 'console'
 import { NextRequest, NextResponse } from 'next/server'
 import React from 'react'
-
+import prisma from '@/prisma/client'
 
 interface Props{
     params:{
-        id:number
+        id: string; // URL params are typically strings
     }
 }
 // how we get an specific Element the GET method api getEntity
-export function GET(request:NextRequest,{ params: { id } }: Props){
-    
-    if(id>10)
+export async function GET(request:NextRequest,{ params: { id } }: Props){
+ 
+    const user = await prisma.user.findUnique({
+        where: {
+          id: parseInt(id), // Ensure id is an integer
+        },
+      });
+    if(!user)
         return NextResponse.json({error:'not found'},{status:404})
     
-    return NextResponse.json({user:'Mosh'},{status:200})
+    return NextResponse.json(user,{status:200})
 }
+
+//I mostly think that it should implemenet the update API in here 
+
+export async function PUT(request:NextRequest,{ params: { id } }: Props){
+  const body=await request.json()
+
+  //check if object Exist in the db
+
+  //update the objects 
+
+  
+}
+
+//the API that delete an Object 
+export function DELETE(request:NextRequest,{ params: { id } }: Props){
+//  search if exist then delete it 
+return NextResponse.json({message:'the Entity deleted !!!'})
+}
+
+//PUT for Replacing Object 
+//Patch for Updating one or more properties
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,18 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+import { error } from "console";
+
+import prisma from '@/prisma/client'
 
 //API method GET to return all data from the database
-export function GET(request:NextRequest){
+export async function GET(request:NextRequest){
     //fetch users from database 
-    return NextResponse.json({data:'Hellow world'});
+    const users=await prisma.user.findMany( )
+
+    return NextResponse.json({users:users});
 }
 
 
 //API to post or save the data to database 
 export async function POST(request:NextRequest){
     //read the body  
+    
     const body=await request.json();
 
-    return NextResponse.json(body)
+    const newUser = await prisma.user.create({
+        data: {
+          email: body.email,
+          name: body.name,
+          followers: body.followers
+          
+        },
+      });
+   
+    return NextResponse.json(newUser)
 
 }
+
+
 
